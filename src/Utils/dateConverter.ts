4a34d9f08a147371133e02a3f85143b95163e99a
MB-Prabhu <prabhukumarmb@gmail.com>
2025-03-12 18:22:25 +0530

    altered dateconverter

--- src/Utils/dateConverter.ts
+++ src/Utils/dateConverter.ts
@@ -4,6 +4,5 @@ export const dateConvertor = (str: string | Date) => {
     let date = String(isDate.getDate()).padStart(2, "0")
     let month = String(isDate.getMonth() + 1).padStart(2, "0")
     let year = isDate.getFullYear()
-    console.log(`${date}/${month}/${year}`)
     return `${date}-${month}-${year}`
 }
